1b89fb688f57ad9c258a397c22728e3bce5d0640
Oluwafemi Sule <oluwafemisule@outlook.com>
2017-09-09 12:16:47 +0100

    setup api router

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const api = require('./api');
 
 const app = express();
+const router = express.Router();
 const port = process.env.PORT || 8080;
 app.use(morgan('tiny'));
 
+app.use('/api', api(router));
 app.use(express.static(path.join(__dirname, 'static')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(path.join(__dirname, 'static'), 'index.html'));
