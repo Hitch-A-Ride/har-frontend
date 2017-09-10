ad2da0a53c306b7fc09da23f5def80143a36a160
Oluwafemi Sule <oluwafemisule@outlook.com>
2017-09-10 08:33:03 +0100

    get service account key from environment

--- server.js
+++ server.js
@@ -14,13 +14,15 @@ const {
   FIREBASE_DATABASE_URL,
   FIREBASE_STORAGE_BUCKET,
   FIREBASE_MESSAGER,
+  FIREBASE_SERVICE_ACCOUNT_KEY
 } = process.env
 
 const app = express();
 const router = express.Router();
 const port = process.env.PORT || 8080;
 
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = JSON.parse(new Buffer(FIREBASE_SERVICE_ACCOUNT_KEY, 'base64').toString());
+
 const fireApp = firebaseAdmin
   .initializeApp({
     credential: firebaseAdmin.credential.cert(serviceAccount),
