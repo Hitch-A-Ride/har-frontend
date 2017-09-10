4c5f53e89fba3f0c13dba4482593776d0bb5903d
Oluwafemi Sule <oluwafemisule@outlook.com>
2017-09-10 02:46:06 +0100

    refactor

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 require('dotenv').config();
 
 const express = require('express');
-const firebase = require('firebase');
 const morgan = require('morgan');
 const path = require('path');
 const firebaseAdmin = require("firebase-admin");
 const bodyParser = require('body-parser');
 
-const { api, getSlackUserId } = require('./api');
-const { 
-  handleSnapshotError,
-  handleSnapshotUpdate,
-} = require('./snapshots');
+const { injectApiDependencies } = require('./api');
+const { setWatchOnDatabaseReferences } = require('./snapshots');
 
 const {
   FIREBASE_AUTH_DOMAIN,
@@ -30,31 +26,21 @@ const fireApp = firebaseAdmin
     credential: firebaseAdmin.credential.cert(serviceAccount),
     databaseURL: FIREBASE_DATABASE_URL,
   });
-
 const database = fireApp.database();
+const watchdog = setWatchOnDatabaseReferences(database);
 
-database
-  .ref('/users')
-  .once('value')
-  .then(handleSnapshotUpdate)
-  .catch(handleSnapshotError);
-  
 
-app.use(bodyParser.json());
 app.use(morgan('tiny'));
-app.use('/api', api(router));
+router.use(bodyParser.urlencoded({
+  extended: true
+}));
+router.use(bodyParser.json());
+app.use('/api', injectApiDependencies(router, watchdog));
 app.use(express.static(path.join(__dirname, 'static')));
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(path.join(__dirname, 'static'), 'index.html'));
 });
-
-
-const messageBroadcast = (req, res, next) => {
-  // broadcast message to slack location for user.
-  // - get country from geocoords;
-  // - post to slack location
-  // https://api.slack.com/methods/chat.postMessage
-}
 
 
 app.listen(port, () => {
